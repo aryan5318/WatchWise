cb6454bb22b723b25244721593856bf262e05aa1
aryan5318 <codev7127@gmail.com>
2025-07-23 08:21:06 +0530

    update image cdn

--- my-app/src/utils/constants.js
+++ my-app/src/utils/constants.js
@@ -5,4 +5,4 @@ export const API_OPTION = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YmNhYjVlZTNjMGVjYTQxYzA3Y2MwN2I4NmZhMmQ4NCIsIm5iZiI6MTczOTY5MjIxMy45MjksInN1YiI6IjY3YjE5OGI1NGU4OWNjZDc4ODZkYWRiNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VTDyPDPwoBieWXh6r6NGAhdKz1pradVx_Am3lBGi0nk'
     }
   };
-  export const IMG_CDN="https:image.tmdb.org/t/p/w500"
+  export const IMG_CDN = "https://image.tmdb.org/t/p/w500";
