a153be4246dd1cf9de8f8a99f34afab4748d3305
aryan5318 <codev7127@gmail.com>
2025-07-23 08:59:56 +0530

    css problem

--- my-app/src/components/movieList.js
+++ my-app/src/components/movieList.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import MovieCard from './movieCard'
+import './movieList.css';  
+
 
 const MovieList = ({title,movies}) => {
     if(movies!=null)
@@ -7,7 +9,8 @@ const MovieList = ({title,movies}) => {
   return (
     <div className='h-96 max-sm:h-40'>
         <h1 className='text-white font-bold ml-28 font-serif absolute mt-10 '>{title}</h1>
-       <div className='flex  relative  overflow-scroll overflow-y-hidden scrollbar-hide  h-96 pl-14'   >
+    <div className='flex relative overflow-x-scroll overflow-y-hidden h-96 pl-14 hide-scrollbar'>
+
        <div className='flex gap-5 max-sm:h-40 overflow-visible h-96 p-14  '>
          {  movies.map(movie=><MovieCard key={movie.id} posterPath={movie.poster_path} id={movie.id} />
         )} 
